7f0af5738f0b83671be866815e65828b59c7570f
Isaías Castrejón <isaias@isaiasmacbookair.local>
2024-03-14 18:47:02 -0600

    Agregué implementación rota de Filter.

--- Practica4/Cumpleannos/src/main/java/kass/concurrente/candadosImpl/Filtro.java
+++ Practica4/Cumpleannos/src/main/java/kass/concurrente/candadosImpl/Filtro.java
@@ -11,6 +11,10 @@ import kass.concurrente.candados.Semaphore;
  * @author Kassandra Mirael
  */
 public class Filtro implements Semaphore {
+    private volatile int[] level;
+    private volatile int[] victim;
+    private int hilos;
+    private int maxHilosConcurrentes;
 
     /**
      * Constructor del Filtro
@@ -18,27 +22,62 @@ public class Filtro implements Semaphore {
      * @param maxHilosConcurrentes EL numero de hilos concurrentes simultaneos
      */
     public Filtro(int hilos, int maxHilosConcurrentes) {
-        /**
-         * AQUI VA TU CODIGO
-         */
+        this.hilos = hilos;
+        this.maxHilosConcurrentes = maxHilosConcurrentes;
+        this.level = new int[hilos];
+        this.victim = new int[hilos];
+        for (int i = 0; i < hilos; i++) {
+            level[i] = 0;
+        }
     }
 
     @Override
     public int getPermitsOnCriticalSection() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPermitsOnCriticalSection'");
+        return this.maxHilosConcurrentes;
     }
 
     @Override
     public void acquire() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'acquire'");
+        int threadId = getThreadId();
+
+        for (int i = 1; i < hilos; i++) {
+            level[threadId] = i;
+            victim[i] = threadId;
+            boolean found;
+            System.out.printf("Thread %d, en ronda %d", threadId, i);
+            do {
+                found = false;
+                for (int k = 0; k < hilos; k++) {
+                    if (k != threadId && level[k] >= i && victim[i] == threadId) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (countThreadsInCriticalSection(i) >= maxHilosConcurrentes) {
+                    found = true;
+                }
+            } while (found);
+        }
     }
 
     @Override
     public void release() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'release'");
+        int threadId = (int) Thread.currentThread().getId() % hilos;
+        level[threadId] = 0; // Leave the critical section, making it available for others
+    }
+
+    private int getThreadId() {
+        return Integer.parseInt(Thread.currentThread().getName());
+    }
+
+    private int countThreadsInCriticalSection(int currentLevel) {
+        int count = 0;
+        for (int lvl : level) {
+            if (lvl >= currentLevel) {
+                count++;
+            }
+        }
+        return count;
     }
     
 }
