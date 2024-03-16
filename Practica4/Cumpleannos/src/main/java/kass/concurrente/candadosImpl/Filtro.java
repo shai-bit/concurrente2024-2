42eefd135b68a5a29f881589f52de36fd5d9b68c
Isaías Castrejón <isaias@isaiasmacbookair.local>
2024-03-16 14:29:19 -0600

    Update Filter con semáforo y lock interno

--- Practica4/Cumpleannos/src/main/java/kass/concurrente/candadosImpl/Filtro.java
+++ Practica4/Cumpleannos/src/main/java/kass/concurrente/candadosImpl/Filtro.java
@@ -13,8 +13,10 @@ import kass.concurrente.candados.Semaphore;
 public class Filtro implements Semaphore {
     private volatile int[] level;
     private volatile int[] victim;
+    private volatile int enSeccionCritica;
     private int hilos;
     private int maxHilosConcurrentes;
+    private volatile boolean lock;
 
     /**
      * Constructor del Filtro
@@ -26,6 +28,8 @@ public class Filtro implements Semaphore {
         this.maxHilosConcurrentes = maxHilosConcurrentes;
         this.level = new int[hilos - 1]; // hilos - 1 porque el último nada más lo usan de verificación en los tests
         this.victim = new int[hilos - 1];
+        this.enSeccionCritica = 0;
+        this.lock = false;
         for (int i = 0; i < hilos - 1; i++) {
             level[i] = 0;
         }
@@ -39,45 +43,62 @@ public class Filtro implements Semaphore {
     @Override
     public void acquire() {
         int threadId = getThreadId();
-
+        
         for (int i = 1; i < hilos - 1; i++) {
+            acquireInternalLock();
             level[threadId] = i;
             victim[i] = threadId;
-            boolean found;
+            releaseInternalLock();
+            boolean conflict;
             do {
-                found = false;
+                conflict = false;
                 for (int k = 0; k < hilos - 1; k++) {
                     if (k != threadId && level[k] >= i && victim[i] == threadId) {
-                        found = true;
+                        conflict = true;
                         break;
                     }
                 }
-                // if (countThreadsInCriticalSection(i) >= maxHilosConcurrentes) {
-                //     found = true;
-                // }
-            } while (found);
+            } while (conflict);
+        }
+
+        while (true) {
+            acquireInternalLock();
+            if (enSeccionCritica < maxHilosConcurrentes) {
+                enSeccionCritica++;
+                releaseInternalLock();
+                break;
+            }
+            releaseInternalLock();
         }
     }
 
     @Override
     public void release() {
         int threadId = getThreadId();
+        acquireInternalLock();
         level[threadId] = 0;
+        enSeccionCritica--;
+        releaseInternalLock();
     }
 
     private int getThreadId() {
         return Integer.parseInt(Thread.currentThread().getName());
     }
 
-    // No estoy seguro si este método nos ayudará para lo de m hilos en la sección crítica
-    private int countThreadsInCriticalSection(int currentLevel) {
-        int count = 0;
-        for (int lvl : level) {
-            if (lvl >= currentLevel) {
-                count++;
+    private void acquireInternalLock() {
+        while (true) {
+            if (!lock) {
+                lock = true;
+                if (!lock) { // checamos dos veces
+                    continue;
+                }
+                break;
             }
         }
-        return count;
+    }
+    
+    private void releaseInternalLock() {
+        lock = false;
     }
     
 }
